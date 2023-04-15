c3d17253ed66bc42f2d240e386208da217c5eb46
Alastair Otter <alastair@mediahack.co.za>
2023-04-15 13:23:55 +0200

    Initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+    import '../styles.css'
+</script>
+
+<main>
+    <div class="sidebar">Sidebar</div>
+<div class="content">
+    <h1>Media Hack Graphic Components</h1>
+    <p>A collection of Svelte components for data visualisation and web-desig.</p>
+    <p>Github: <a href="https://github.com/mediahackza/graphic-components">https://github.com/mediahackza/graphic-components</a> </p>
+    
+    <slot />
+</div>
+</main>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script>
+    import { Button } from '@mediahackza/graphic-components'
+</script>
+
+<h2>Button</h2>
+<Button />
+
+
